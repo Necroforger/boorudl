87b5f2fb3137f75e805938cae9546bbd73a5825c
Rinzen Necroforger <Necroforger@gmail.com>
2017-05-20 01:05:26 -0400

    Remove cirno from default value

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func ParseFlags() {
 	flagset.StringVar(&OutputDir, "o", "", "Output directory for downloaded files")
 	flagset.IntVar(&Page, "p", 1, "Page to start downloading from.")
 	flagset.IntVar(&Limit, "l", 1, "Maximum number of images to download")
-	flagset.StringVar(&Tags, "t", "cirno", "Space separated tags to search for")
+	flagset.StringVar(&Tags, "t", "", "Space separated tags to search for")
 	flagset.BoolVar(&Random, "r", false, "Specifies if the result should be random. Only works on danbooru")
 
 	if len(os.Args) < 3 {
