d03a1e74e358fba3ff961c46462bc85f297a395c
Rinzen Necroforger <Necroforger@gmail.com>
2017-05-21 21:22:16 -0400

    Skeleton for UI

--- ui/main.go
+++ ui/main.go
@@ -1,6 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	astilectron "github.com/Necroforger/go-astilectron"
+)
+
+var (
+	workingDirectory string
+	electron         *astilectron.Astilectron
+	window           *astilectron.Window
+)
 
 func main() {
-	robotgo.MoveMouse(200, 290)
+
+	var err error
+	workingDirectory, err = os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// TODO Create application icons.
+	if electron, err = astilectron.New(astilectron.Options{
+		AppName:            "Boorudl",
+		AppIconDarwinPath:  "",
+		AppIconDefaultPath: "",
+		BaseDirectoryPath:  workingDirectory,
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer electron.Close()
+	electron.HandleSignals()
+
+	// Attempt to start the electron session.
+	// If electron does not exist, download it.
+	if err = electron.Start(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if window, err = electron.NewWindow(workingDirectory+"/index.html", &astilectron.WindowOptions{
+		Width:  astilectron.PtrInt(800),
+		Height: astilectron.PtrInt(600),
+		Center: astilectron.PtrBool(true),
+	}); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err = window.Create(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	electron.Wait()
 }
